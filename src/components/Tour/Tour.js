c046302bafb754c4a47f5074e58c4a009548a174
kryptondv <kryptondevpl@gmail.com>
2020-06-25 10:45:37 +0200

    add Tour component

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import './tour.scss'
 
-const Tour = () => {
+const Tour = ({ tour, removeTour }) => {
+  const { id, city, img, name, info } = tour;
+
   return (
-    <div>
-      TOur
-    </div>
-  )
+    <article className="tour">
+      <div className="img-container">
+        <img src={img} alt={name} />
+        <span className="close-btn">
+          <i className="fas fa-window-close"></i>
+        </span>
+      </div>
+      <div className="tour-info">
+        <h3>{city}</h3>
+        <h4>{name}</h4>
+        <h5>info{""}
+          <span>
+            <i className="fas fa-caret-square-down"></i>
+          </span>
+        </h5>
+      <p>{info}</p>
+      </div>
+    </article>
+  );
 }
 
 export default Tour
